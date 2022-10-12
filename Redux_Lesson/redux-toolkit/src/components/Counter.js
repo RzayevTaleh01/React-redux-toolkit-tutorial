22da1fb908d87c7ea8b266018b2bbf11c125d806
RzayevTaleh01 <taleh.rzayev.i@asoiu.edu.az>
2022-10-12 19:40:20 +0400

    test repo

--- Redux_Lesson/redux-toolkit/src/components/Counter.js
+++ Redux_Lesson/redux-toolkit/src/components/Counter.js
@@ -6,4 +6,5 @@ export default function Counter() {
     const count = useSelector((state)=>state.counter.value) //storeden statei cekmek ucun useSelector istifade edilir
 
   return <div>{count}</div>;
+  
 }
