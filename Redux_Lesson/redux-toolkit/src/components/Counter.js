c1798173b5dacafc08d0fe93a5d9e9029a2a3b77
RzayevTaleh01 <taleh.rzayev.i@asoiu.edu.az>
2022-10-12 20:09:45 +0400

    Counter app with Redux toolkit is completed

--- Redux_Lesson/redux-toolkit/src/components/Counter.js
+++ Redux_Lesson/redux-toolkit/src/components/Counter.js
@@ -2,9 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 export default function Counter() {
-
-    const count = useSelector((state)=>state.counter.value) //storeden statei cekmek ucun useSelector istifade edilir
+  const count = useSelector((state) => state.counter_store.value); //storeden statei cekmek ucun useSelector istifade edilir
 
   return <div>{count}</div>;
-  
 }
--- Redux_Lesson/redux-toolkit/src/store.js
+++ Redux_Lesson/redux-toolkit/src/store.js
@@ -2,6 +2,6 @@ import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from "./stores/CounterSlice"
 export const store = configureStore({
   reducer: {
-    counter: counterReducer
+    counter_store: counterReducer //counter store qoyma meqsedim sadece actionsdaki valuenin hardan geldiyini yoxlamaqdir
   },
 })
