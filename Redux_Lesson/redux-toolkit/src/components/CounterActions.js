a9cff062fbab19258776ee80c0ccc7ce2a510b8a
RzayevTaleh01 <taleh.rzayev.i@asoiu.edu.az>
2022-10-12 19:48:42 +0400

    Complete inc and decr operations with useDispatch

--- Redux_Lesson/redux-toolkit/src/components/CounterActions.js
+++ Redux_Lesson/redux-toolkit/src/components/CounterActions.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { increment,decrement } from "../stores/Counter";
 
-export default function CounterActions({ setCount, count }) {
+export default function CounterActions() {
+    const dispatch = useDispatch(); //Slicedaki reducerleri cekmek ucun
   return (
     <div>
-      <button onClick={() => setCount(count + 1)}>Artir</button>
-      <button onClick={() => setCount(count - 1)}>Azalt</button>
+      <button onClick={() => dispatch(increment())}>Artir</button>
+      <button onClick={() => dispatch(decrement())}>Azalt</button>
     </div>
   );
 }
