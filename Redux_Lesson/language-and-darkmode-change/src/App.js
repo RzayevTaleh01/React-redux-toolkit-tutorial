8f172a7cc2520a43e0ddd5add14af1a3a1206b6b
RzayevTaleh01 <taleh.rzayev.i@asoiu.edu.az>
2022-10-13 19:56:54 +0400

    dark mode is completed

--- Redux_Lesson/language-and-darkmode-change/src/App.js
+++ Redux_Lesson/language-and-darkmode-change/src/App.js
@@ -1,10 +1,16 @@
+import { useSelector } from "react-redux";
 import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 function App() {
+
+  const {dark} = useSelector(state => state.site)
+
+
+
   return (
-    <div className="App">
+    <div className={dark ? "App dark":"App"}>
       <Header />
       <Footer/>
     </div>
